3b39cd01dee9ed04f6c7f1c409eab84021f2a63e
yamamoto kohei <yamamoto.kohei@vareal.co.jp>
2019-03-06 18:59:01 +0900

    finish user edit,update,index,and destroy actions

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -9,6 +9,12 @@ module SessionHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -41,5 +47,15 @@ module SessionHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   before_save {self.email=email.downcase}
   validates :name,presence: true,length:{maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email,presence: true,length:{maximum:25},
+  validates :email,presence: true,length:{maximum:255},
                   format:{with: VALID_EMAIL_REGEX},
                   uniqueness: {case_sensitive: false}
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 },allow_nil:true
 
   class<<self
   def digest(string)
