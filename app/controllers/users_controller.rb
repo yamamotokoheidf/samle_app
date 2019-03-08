714dd5bde1a696621b3bf889d5871c01380dc24c
yamamoto kohei <yamamoto.kohei@vareal.co.jp>
2019-03-08 13:27:30 +0900

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [:index, :edit, :update,:destroy]
+    before_action :logged_in_user, only: [:index, :edit, :update,:destroy,:following,:follower]
   before_action:logged_in_user, only:[:edit,:update]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
@@ -58,7 +58,7 @@ end
                                    :password_confirmation)
     end
 
-    
+
 
     def correct_user
       @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,13 @@ Rails.application.routes.draw do
   get '/login'=>'session#new'
   post '/login'=>'session#create'
   delete '/logout'=>'session#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following,:followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only:[:new,:create,:edit,:update]
   resources :microposts, only:[:create,:destroy]
+  resources :relationships, only: [:create,:destroy]
 end
